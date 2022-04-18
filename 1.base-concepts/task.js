e09e1b42936203967bdbbe830f0c1805bb230e0c
Techworks Dev Team <99633958+TechworksDev@users.noreply.github.com>
2022-04-18 10:39:09 +0600

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,11 +3,3 @@ function solveEquation(a, b, c) {
   // код для задачи №1 писать здесь
   return arr; // array
 }
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
