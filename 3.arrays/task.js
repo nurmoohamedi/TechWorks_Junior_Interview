9a9901d6c4323b6ceb4e40a23ff492fc0829dab3
nurmoohamedi <nurmoohamedi@gmail.com>
2022-06-13 23:26:37 +0600

    solved Array tests

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,28 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+    let result = (arr1, arr2) => {
 
-  return result; // boolean
+        if (arr1.length !== arr2.length)
+            return false;
+
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i])
+                return false;
+        }
+
+        return true;
+    };
+
+    return result(arr1, arr2); // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr = [];
 
-  // Ваш код
+    arr.forEach(item => {
+        if (item > 0 && item % 3 === 0)
+            resultArr.push(item*10);
+    })
 
-  return resultArr; // array
+    return resultArr; // array
 }
