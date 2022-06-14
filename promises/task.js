49d7f6c96f609f9cae8d1d5f4d1ffabdd224d443
nurmoohamedi <nurmoohamedi@gmail.com>
2022-06-14 06:16:44 +0600

    updated test

--- promises/task.js
+++ promises/task.js
@@ -1,5 +1,5 @@
 const result = promiseFunction()
 
-result.then(resolve => {
+result.then(resolve =>
     console.log(`Промис ${typeof resolve === 'string' ? '' : 'НЕ'} работает ${typeof resolve === 'string' ? resolve : ''}`)
-})
+)
